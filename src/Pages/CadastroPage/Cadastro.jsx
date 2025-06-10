7a833adc9ec1197c130435d6bda1f2d646b7fe4f
GABRIEL LINO VASCONCELLOS DOS SANTOS <gabriel_lv_santos@estudante.sesisenai.org.br>
2025-06-10 17:23:25 -0300

    primeiro modelo

---
+++ src/Pages/CadastroPage/Cadastro.jsx
@@ -0,0 +1,17 @@
+import React, { use } from "react";
+import { useState } from "react";
+
+const [name, setName] = useState()
+const [email, setEmail] = useState()
+const [password, setPassword] = useState()
+
+const Cadastro = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+
+
---
+++ src/Pages/LoginPage/Login.jsx
@@ -0,0 +1,21 @@
+import React, { use } from "react";
+import { useState } from "react";
+
+const [email, setEmail] = useState()
+const [password, setPassword] = useState()
+
+
+
+
+const Login = () => {
+    return(
+        <section>
+            <form>
+
+            </form>
+        </section>
+    )
+}
+
+
+
